316560ac8bf3585208d5202cd959f244d4d52763
Hash <whatsuphash2001@gmail.com>
2022-11-08 14:16:09 +0500

    12222

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,32 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
+import New from './new';
 
 test('renders learn react link', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  const linkElement = screen.getByTestId("comp");
+  
+  expect(linkElement).toHaveTextContent("hello");
+
+
+
 });
+
+test('renders learn react link', () => {
+  render(<New />);
+  const linkElement = screen.getByTestId("comp1");
+  
+  expect(linkElement).toHaveTextContent("2") });
+
+
+  
+
+  test('renders learn react link', () => {
+    render(<App />);
+    const linkElement = screen.getByTestId("comp");
+    
+    expect(linkElement).toBeInTheDocument();
+  
+  
+  
+  });
---
+++ src/new.js
@@ -0,0 +1,24 @@
+import logo from './logo.svg';
+
+
+function New() {
+  return (
+    <div data-testid="comp1" >
+      <h1>1</h1>
+
+      <h2>2</h2>
+      <h3>3</h3>
+      <h4>4</h4>
+     
+      
+    </div>
+  );
+}
+
+export default New;
+
+
+
+
+
+
